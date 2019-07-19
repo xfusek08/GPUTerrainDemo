dcd5c75c72a744bcef45a4f54ec3098cf97fdec7
Petr Fusek <xfusek08@stud.fit.vutbr.cz>
2019-07-19 19:50:22 +0200

    sdl controls separated from orbit camera controller

--- src/TerrainDemo/tdsdl/SDLOrbitCameraController.cpp
+++ src/TerrainDemo/tdsdl/SDLOrbitCameraController.cpp
@@ -1,28 +1,16 @@
 
-#include <glm/glm.hpp>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/quaternion.hpp>
-
 #include <TerrainDemo/tdsdl/SDLOrbitCameraController.h>
-
-#ifdef GT_DEBUG
-#define GLM_ENABLE_EXPERIMENTAL
-#include <glm/gtx/string_cast.hpp>
-#include <TerrainDemo/core/Utils.h>
-#endif
 
 using namespace std;
 using namespace TerrainDemo::tdsdl;
 using namespace TerrainDemo::core;
 
 SDLOrbitCameraController::SDLOrbitCameraController(shared_ptr<Camera> camera) :
-    _camera(camera),
-    _speed(0.0005f),
-    _radius(5.f)
+    OrbitCameraController::OrbitCameraController(camera)
 {
-    _camera->setCameraPosition(glm::vec3{0,0,1});
+    _speed = 0.0005f;
+    _radius = 5.f;
+    _camera->setCameraPosition(glm::vec3{ 0,0,1 });
     performRotation();
 }
 
@@ -44,63 +32,4 @@ void SDLOrbitCameraController::onFrameUpdate()
 
     if (state[SDL_SCANCODE_S])          zoomOut();
     else if (state[SDL_SCANCODE_W])     zoomIn();
-}
-
-void SDLOrbitCameraController::up()
-{
-    performRotation(
-        _speed,
-        glm::normalize(glm::cross(_camera->getCameraDirection(), glm::vec3{ 0.f,1.f,0.f }))
-    );
-}
-
-void SDLOrbitCameraController::down()
-{
-    performRotation(
-        -_speed,
-        glm::normalize(glm::cross(_camera->getCameraDirection(), glm::vec3{ 0.f,1.f,0.f }))
-    );
-}
-
-void SDLOrbitCameraController::left()
-{
-    performRotation(
-        -_speed,
-        glm::vec3(0.f, 1.f, 0.f)
-    );
-}
-
-void SDLOrbitCameraController::right()
-{
-    performRotation(
-        _speed,
-        glm::vec3(0.f, 1.f, 0.f)
-    );
-}
-
-void SDLOrbitCameraController::zoomIn()
-{
-    _radius *= 0.99f;
-    performRotation();
-}
-
-void SDLOrbitCameraController::zoomOut()
-{
-    _radius *= 1.01f;
-    performRotation();
-}
-
-void SDLOrbitCameraController::lookXY(glm::vec2 delta)
-{
-}
-
-void SDLOrbitCameraController::performRotation(float angleIncrement, glm::vec3 axis)
-{
-    auto pos = _camera->getCameraPosition();
-    if (angleIncrement != 0 || axis == glm::vec3(0)) {
-        glm::quat rot = glm::angleAxis(angleIncrement, axis);
-        pos = glm::conjugate(rot) * pos * rot;
-    }
-    pos = glm::normalize(pos) * _radius;
-    _camera->setCameraPosition(pos);
 }
