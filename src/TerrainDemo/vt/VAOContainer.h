1ad6e524609db4de571b0cbef07e09298f14eb21
Petr Fusek <xfusek08@stud.fit.vutbr.cz>
2020-02-01 19:22:20 +0100

    polish an cpp propertiers

--- src/TerrainDemo/vt/VAOContainer.h
+++ src/TerrainDemo/vt/VAOContainer.h
@@ -21,14 +21,15 @@ namespace TerrainDemo
         class VAOContainer {
         public:
             std::shared_ptr<ge::gl::VertexArray> vao;
-			GLsizei indexSize;			
+			GLsizei indexSize;
 
             VAOContainer(std::shared_ptr<ge::gl::Context> gl);
 
             void bind();
             void unbind();
 
-			template<typename T> std::shared_ptr<ge::gl::Buffer> newBuffer(std::vector<T> values)
+			template<typename T>
+            std::shared_ptr<ge::gl::Buffer> newBuffer(std::vector<T> values)
 			{
 				auto buffer = std::make_shared<ge::gl::Buffer>(values.size() * sizeof(T), values.data());
 				_bufferContainer.push_back(buffer);
@@ -36,7 +37,7 @@ namespace TerrainDemo
 			}
 
 		protected:
-            std::vector<std::shared_ptr<ge::gl::Buffer>> _bufferContainer;            
+            std::vector<std::shared_ptr<ge::gl::Buffer>> _bufferContainer;
 		};
     } // namespace vt
 } // namespace TerrainDemo
